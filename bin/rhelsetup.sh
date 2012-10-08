0a16a5f7c8c454013da08abb6a7774045cfd8499
Daniel Hall <daniel@danielhall.me>
2012-10-08 15:53:14 +1100

    Setup script for new RHEL nodes

---
+++ bin/rhelsetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rhelhost="$1"
+PUBLIC='keys/id_rsa.pub'
+
+if [ ! -r $PUBLIC ]; then
+  echo 'COuld not find public key'
+fi
+
+ssh-copy-id -i $PUBLIC ${rhelhost}
+ssh ${rhelhost} -- yum -y install python-simplejson
+
