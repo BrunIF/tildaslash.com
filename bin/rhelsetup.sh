bb6f48a215b8f8e5dc744501075ee1f6b1be3c06
Daniel Hall <daniel@danielhall.me>
2012-10-09 16:04:28 +1100

    Add the keys to the root user

--- bin/rhelsetup.sh
+++ bin/rhelsetup.sh
@@ -3,10 +3,10 @@
 rhelhost="$1"
 PUBLIC='keys/id_rsa.pub'
 
-if [ ! -r $PUBLIC ]; then
+if [ ! -r "${PUBLIC}" ]; then
   echo 'COuld not find public key'
 fi
 
-ssh-copy-id -i $PUBLIC ${rhelhost}
+ssh-copy-id -i ${PUBLIC} root@${rhelhost}
 ssh ${rhelhost} -- yum -y install python-simplejson
 
