82914865102b185fa3a72251e20597377488cb3e
Daniel Hall <daniel@danielhall.me>
2012-11-06 16:50:15 +1100

    Support both temps at once

--- playbooks/website/fridge/temp.php
+++ playbooks/website/fridge/temp.php
@@ -2,7 +2,9 @@
 
 if (array_key_exists('tempA', $_GET)) {
   file_put_contents('data/tempA', $_GET['tempA']);
-} else if (array_key_exists('tempB', $_GET)) {
+}
+
+if (array_key_exists('tempB', $_GET)) {
   file_put_contents('data/tempB', $_GET['tempB']);
 }
 
