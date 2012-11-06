1f18cddbdea9e6142dc1f4b0d3cbc87790b8cd3b
Daniel Hall <daniel@danielhall.me>
2012-11-06 16:35:29 +1100

    Fridge stuff

--- playbooks/website/fridge/temp.php
+++ playbooks/website/fridge/temp.php
@@ -1,1 +1,9 @@
-Y
+<?php
+
+if (array_key_exists('tempA', $_GET)) {
+  file_put_contents('data/tempA', $_GET['tempA']);
+} else if (array_key_exists('tempB', $_GET)) {
+  file_put_contents('data/tempB', $_GET['tempB']);
+}
+
+?>Y
