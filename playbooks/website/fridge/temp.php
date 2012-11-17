e2db5664bb900d3bb479eba83b5b6119ce88e4ce
Daniel Hall <daniel@danielhall.me>
2012-11-17 22:51:23 +1100

    New weight sensor in fridge

--- playbooks/website/fridge/temp.php
+++ playbooks/website/fridge/temp.php
@@ -8,4 +8,8 @@ if (array_key_exists('tempB', $_GET)) {
   file_put_contents('data/tempB', $_GET['tempB']);
 }
 
+if (array_key_exists('weight', $_GET)) {
+  file_put_contents('data/weight', $_GET['weight']);
+}
+
 ?>Y
