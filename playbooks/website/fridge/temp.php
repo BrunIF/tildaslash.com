8325e30700657f1759c91103df87800fde31be61
Daniel Hall <daniel@danielhall.me>
2012-11-18 10:59:02 +1100

    Store data also using rrdtool

--- playbooks/website/fridge/temp.php
+++ playbooks/website/fridge/temp.php
@@ -1,15 +1,50 @@
 <?php
 
+if (!file_exists('data/tempA.rrd')) {
+  $options = array(
+     '--step', '10',
+     'DS:tempa:GAUGE:10:-40:100',
+     'RRA:AVERAGE:0.5:6:1440',
+     'RRA:AVERAGE:0.5:360:168'
+  );
+
+  rrd_create('data/tempA.rrd', $options, count($options));
+}
+
+if (!file_exists('data/tempB.rrd')) {
+  $options = array(
+     '--step', '10',
+     'DS:tempb:GAUGE:10:-40:100',
+     'RRA:AVERAGE:0.5:6:1440',
+     'RRA:AVERAGE:0.5:360:168'
+  );
+
+  rrd_create('data/tempB.rrd', $options, count($options));
+}
+
+if (!file_exists('data/weight.rrd')) {
+  $options = array(
+     '--step', '10',
+     'DS:weight:GAUGE:10:0:1024',
+     'RRA:AVERAGE:0.5:6:1440',
+     'RRA:AVERAGE:0.5:360:168'
+  );
+
+  rrd_create('data/weight.rrd', $options, count($options));
+}
 if (array_key_exists('tempA', $_GET)) {
   file_put_contents('data/tempA', $_GET['tempA']);
+  rrd_update('data/tempA.rrd', 'N:' . $_GET['tempA'])
 }
 
 if (array_key_exists('tempB', $_GET)) {
   file_put_contents('data/tempB', $_GET['tempB']);
+  rrd_update('data/tempB.rrd', 'N:' . $_GET['tempB'])
 }
 
 if (array_key_exists('weight', $_GET)) {
   file_put_contents('data/weight', $_GET['weight']);
+  rrd_update('data/weight.rrd', 'N:' . $_GET['weight'])
 }
 
 ?>Y
